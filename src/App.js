08fac7796b7bd0184b418ae02357e5b1dafb456d
Murat Artan <muratartan@hotmail.com>
2021-03-20 00:54:14 +0300

    set header to App.js so it is visible in all pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
 import NewPlace from './places/pages/NewPlace';
 import Users from './user/pages/Users';
+import MainNavigation from './shared/components/navigation/MainNavigaton/MainNavigation';
 
 const App = () => {
-   return <Router>
-      <Switch>
-         <Route path='/' exact>
-            <Users />
-         </Route>
-         <Route path='/places/new' exact>
-            <NewPlace />
-         </Route>
-         <Redirect to='/' />
-      </Switch>
-   </Router>
+   return (
+      <Router>
+         <MainNavigation />
+         <main>
+         <Switch>
+            <Route path='/' exact>
+               <Users />
+            </Route>
+            <Route path='/places/new' exact>
+               <NewPlace />
+            </Route>
+            <Redirect to='/' />
+         </Switch>
+         </main>
+      </Router>)
 };
 
 export default App;
