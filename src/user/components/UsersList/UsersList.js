30c41310c59b4c48ebb8614e4d4c8023b448374f
Murat Artan <muratartan@hotmail.com>
2021-03-19 23:15:18 +0300

    bug fix

--- src/user/components/UsersList/UsersList.js
+++ src/user/components/UsersList/UsersList.js
@@ -1,27 +1,30 @@
 import React from 'react';
 
-import './UsersList.css';
 import UserItem from '../UserItem/UserItem';
+import './UsersList.css';
 
 const UsersList = props => {
-   if (props.items.length === 0) {
-      return (
-         <div className='center'>
-            <h2>No users found.</h2>
-         </div>
-      );
-   }
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <h2>No users found.</h2>
+      </div>
+    );
+  }
 
-   return <ul className='users-list'>
+  return (
+    <ul className="users-list">
       {props.items.map(user => (
-         <UserItem
-            key={user.id}
-            id={user.id}
-            image={user.image}
-            name={user.name}
-            placeCount={user.places} />
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
       ))}
-   </ul>
+    </ul>
+  );
 };
 
 export default UsersList;
