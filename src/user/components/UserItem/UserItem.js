bbc8e66a74f2baed1ad0beefeff35c1498acf1ef
Murat Artan <muratartan@hotmail.com>
2021-03-19 23:48:02 +0300

    set link to user item to enter a user profile

--- src/user/components/UserItem/UserItem.js
+++ src/user/components/UserItem/UserItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import './UserItem.css';
 import Avatar from '../../../shared/components/UIElements/Avatar/Avatar';
@@ -7,6 +8,7 @@ const UserItem = props => {
   return (
     <li className="user-item">
       <div className="user-item__content">
+          <Link to={`/${props.id}/places`}>
         <div className="user-item__image">
          <Avatar image={props.image} alt={props.name} />
         </div>
@@ -16,6 +18,7 @@ const UserItem = props => {
             {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
           </h3>
         </div>
+        </Link>
       </div>
     </li>
   );
