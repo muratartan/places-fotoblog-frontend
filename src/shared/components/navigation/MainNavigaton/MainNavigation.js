90daf692812a3c1da1c8fdbf09e900641c19e668
Murat Artan <muratartan@hotmail.com>
2021-03-20 12:21:20 +0300

    set NavLinks in MainNavigation component

--- src/shared/components/navigation/MainNavigaton/MainNavigation.js
+++ src/shared/components/navigation/MainNavigaton/MainNavigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import MainHeader from '../MainHeader/MainHeader';
+import NavLinks from '../NavLinks/NavLinks';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
@@ -15,7 +16,7 @@ const MainNavigation = props => {
             <Link to='/'>Your Places</Link>
         </h1>
         <nav>
-            ...
+            <NavLinks />
         </nav>
     </MainHeader>
 };
