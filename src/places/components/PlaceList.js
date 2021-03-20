8269bec804680ef087bac9e42982b334b95f6155
Murat Artan <muratartan@hotmail.com>
2021-03-20 22:25:21 +0300

    set PlaceItem in  PlaceList component if there's a place item

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,17 +1,34 @@
 import React from 'react';
 
 import Card from '../../shared/components/UIElements/Card/Card';
+import PlaceItem from './PlaceItem';
 import './PlaceList.css';
 
 const PlaceList = props => {
-    if (props.items.length === 0) {
-        return <div className='place-list center'>
-            <Card>
+   if (props.items.length === 0) {
+      return (<div className='place-list center'>
+         <Card>
             <h2>No places found. Maybe add one?</h2>
             <button>share place</button>
-            </Card>
-        </div>
-    }
+         </Card>
+      </div>
+      );
+   }
+
+   return <ul className='place-list'>
+      {props.items.map(place => (
+         <PlaceItem
+            key={place.id}
+            id={place.id}
+            image={place.imageUrl}
+            title={place.title}
+            description={place.description}
+            address={place.address}
+            creatorId={place.creator}
+            coordinates={place.location}
+         />
+      ))}
+   </ul>
 };
 
 export default PlaceList;
