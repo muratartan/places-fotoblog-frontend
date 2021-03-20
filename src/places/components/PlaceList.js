8eb453ac02c27f300d6580810a3dab4fe04e7022
Murat Artan <muratartan@hotmail.com>
2021-03-20 22:13:05 +0300

    show message if no places found

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import Card from '../../shared/components/UIElements/Card/Card';
 import './PlaceList.css';
 
-const PlaceList = props => {};
+const PlaceList = props => {
+    if (props.items.length === 0) {
+        return <div className='place-list center'>
+            <Card>
+            <h2>No places found. Maybe add one?</h2>
+            <button>share place</button>
+            </Card>
+        </div>
+    }
+};
 
 export default PlaceList;
