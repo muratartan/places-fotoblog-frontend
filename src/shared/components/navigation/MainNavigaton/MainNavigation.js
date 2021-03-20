df34c185eea3fc78ca0aa5b2d75444eacb86f754
Murat Artan <muratartan@hotmail.com>
2021-03-20 19:05:53 +0300

    set side drawer is open and closed functionality

--- src/shared/components/navigation/MainNavigaton/MainNavigation.js
+++ src/shared/components/navigation/MainNavigaton/MainNavigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from '../MainHeader/MainHeader';
@@ -7,15 +7,27 @@ import SideDrawer from '../SideDrawer/SideDrawer';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
+   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+   const openDrawer = () => {
+      setDrawerIsOpen(true);
+   };
+
+   const closeDrawer = () => {
+      setDrawerIsOpen(false);
+   };
+
    return (
       <React.Fragment>
+         {drawerIsOpen && (
          <SideDrawer>
             <nav className='main-navigation__drawer-nav'>
                <NavLinks />
             </nav>
          </SideDrawer>
+         )}
          <MainHeader>
-            <button className='main-navigation__menu-btn'>
+            <button className='main-navigation__menu-btn' onClick={openDrawer}>
                <span />
                <span />
                <span />
