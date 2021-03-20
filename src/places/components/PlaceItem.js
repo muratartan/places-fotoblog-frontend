a5ab9902a60d2526635c5672066cd8fe3d94465c
Murat Artan <muratartan@hotmail.com>
2021-03-20 22:33:18 +0300

    set PlaceItem content

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -2,6 +2,24 @@ import React from 'react';
 
 import './PlaceItem.css';
 
-const PlaceItem = props => {};
+const PlaceItem = props => {
+   return (
+      <li className='place-item'>
+         <div className='place-item__image'>
+            <img src={props.image} alt={props.title} />
+         </div>
+         <div className='place-item__info'>
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+         </div>
+         <div className='place-item__actions'>
+            <button>VIEW ON MAP</button>
+            <button>VIEW ON MAP</button>
+            <button>DELETE</button>
+         </div>
+      </li>
+   );
+};
 
 export default PlaceItem;
