bf5a76e42ff1da13676b48f40fe3a46b59cb257f
Murat Artan <muratartan@hotmail.com>
2021-03-25 23:35:28 +0300

    add UserPlaces to PlaceList, create a dummy places data and set in PlaceList component as props

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -17,7 +17,7 @@ const PlaceItem = props => {
             </div>
             <div className='place-item__actions'>
                <button>VIEW ON MAP</button>
-               <button>VIEW ON MAP</button>
+               <button>EDIT</button>
                <button>DELETE</button>
             </div>
          </Card>
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,5 +1,36 @@
 import React from 'react';
 
-const UserPlaces = props => {};
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        imageUrl: 'https://media.timeout.com/images/101705309/image.jpg',
+        address: '20 W 34th Street, New york',
+        location: {
+            lat: 40.748432,
+            lng: -73.985656
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        imageUrl: 'https://media.timeout.com/images/101705309/image.jpg',
+        address: '20 W 34th Street, New york',
+        location: {
+            lat: 40.748432,
+            lng: -73.985656
+        },
+        creator: 'u2'
+    }
+];
+
+const UserPlaces = props => {
+    return <PlaceList items={DUMMY_PLACES} />
+};
 
 export default UserPlaces;
