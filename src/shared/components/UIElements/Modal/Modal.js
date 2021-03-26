5799883dfff8fe55f60370cda61ddd35756a7571
Murat Artan <muratartan@hotmail.com>
2021-03-26 19:45:25 +0300

    create modaloverlay component in Modal.js and set "modal-hook" portal

--- src/shared/components/UIElements/Modal/Modal.js
+++ src/shared/components/UIElements/Modal/Modal.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './Modal.css';
+
+const ModalOverlay = props => {
+    ReactDOM.createPortal(, document.getElementById('modal-hook'));
+};
 
 const Modal = props => {};
 
