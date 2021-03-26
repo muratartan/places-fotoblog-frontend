87303f5937b749bb363841614196d344fb1b6ba2
Murat Artan <muratartan@hotmail.com>
2021-03-26 19:52:17 +0300

    create portal content element and set header

--- src/shared/components/UIElements/Modal/Modal.js
+++ src/shared/components/UIElements/Modal/Modal.js
@@ -4,7 +4,14 @@ import ReactDOM from 'react-dom';
 import './Modal.css';
 
 const ModalOverlay = props => {
-    ReactDOM.createPortal(, document.getElementById('modal-hook'));
+    const content = (
+        <div className={`modal ${props.className}`} style={props.style}>
+            <header className={`modal_header ${props.headerClass}`}>
+                <h2>{props.header}</h2>
+            </header>
+        </div>
+    );
+    ReactDOM.createPortal(content, document.getElementById('modal-hook'));
 };
 
 const Modal = props => {};
