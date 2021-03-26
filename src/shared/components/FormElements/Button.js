2fcd36954908fc4d16cbb7adb394e26cebc8db35
Murat Artan <muratartan@hotmail.com>
2021-03-26 18:25:56 +0300

    render a normal link if have a href props

--- src/shared/components/FormElements/Button.js
+++ src/shared/components/FormElements/Button.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 import './Button.css';
 
-const Button = props => {};
+const Button = props => {
+    if (props.href) {
+        return (
+            <a className={`button button--${props.size || 'default'} ${props.inverse && 
+                'button--inverse'} ${props.danger && 'button--danger'}`}
+                     href={props.href} >
+                {props.children}
+            </a>
+        );
+    }
+};
 
 export const Button;
