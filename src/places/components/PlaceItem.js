5e6632b9a221e442fb393e5a8cf637b2e634ee74
Murat Artan <muratartan@hotmail.com>
2021-03-26 22:10:45 +0300

    create functions that show or not show the Modal

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Card from '../../shared/components/UIElements/Card/Card';
 import Button from '../../shared/components/FormElements/Button';
+import Modal from '../../shared/components/UIElements/Modal/Modal';
 import './PlaceItem.css';
 
 const PlaceItem = props => {
+   const [showMap, setShowMap] = useState(false);
+
+   const openMapHandler = () => setShowMap(true);
+
+   const closeMapHandler = () => setShowMap(false);
+
    return (
       <li className='place-item'>
          <Card className='place-item__content'>
