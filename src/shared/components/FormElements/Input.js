8bcacd69d112815d168f14359375fece3f18e91b
Murat Artan <muratartan@hotmail.com>
2021-03-27 12:50:28 +0300

    add onChange events

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -3,11 +3,14 @@ import React from 'react';
 import './Input.css';
 
 const Input = props => {
-    const element = props.element === 'input' ? ( <input id={props.id} 
-    type={props.type}
-     placeholder={props.placeholder}
-      />) : ( 
-      <textarea id={props.id} rows={props.rows || 3} /> );
+    const changeHandler = event => { };
+
+    const element = props.element === 'input' ? (<input id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+    />) : (
+        <textarea id={props.id} rows={props.rows || 3} onChange={changeHandler} />);
 
     return <div className={`form-control`}>
         <label htmlFor={props.id}>{props.label}</label>
