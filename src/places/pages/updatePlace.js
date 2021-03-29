4a65bda2c4a29ce1e4a900a26bee9f4759b347bd
Murat Artan <muratartan@hotmail.com>
2021-03-29 23:50:05 +0300

    find place id from data

--- src/places/pages/updatePlace.js
+++ src/places/pages/updatePlace.js
@@ -1,6 +1,44 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        imageUrl: 'https://media.timeout.com/images/101705309/image.jpg',
+        address: '20 W 34th Street, New york',
+        location: {
+            lat: 40.748432,
+            lng: -73.985656
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        imageUrl: 'https://media.timeout.com/images/101705309/image.jpg',
+        address: '20 W 34th Street, New york',
+        location: {
+            lat: 40.748432,
+            lng: -73.985656
+        },
+        creator: 'u2'
+    }
+];
 
 const UpdatePlace = () => {
+    const placeId = useParams().placeId;
+
+    const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+
+    if (!identifiedPlace) {
+        return <div className='center'>
+            <h2>Could not found place!</h2>
+        </div>
+    }
+
     return <h2>Update Place</h2>
 };
 
