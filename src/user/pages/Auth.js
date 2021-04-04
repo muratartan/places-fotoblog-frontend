4120904f7212e20e4b7dc7cc15576560901b5cf1
Murat Artan <muratartan@hotmail.com>
2021-04-04 21:58:11 +0300

    set Card in Auth

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Card from '../../shared/components/UIElements/Card/Card';
 import './Auth.css';
 
 const Auth = props => {
-    <h2>Auth!</h2>
+    return <Card className='authentication'>
+        <h2>Login required!</h2>
+        <hr/>
+        <form></form>
+    </Card>
 };
 
 export default Auth;
