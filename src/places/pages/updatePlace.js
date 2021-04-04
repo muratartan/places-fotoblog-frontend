48d87fb01a383b6c056c9692fba5de930291be07
Murat Artan <muratartan@hotmail.com>
2021-04-04 18:29:11 +0300

    Update updatePlace.js

--- src/places/pages/updatePlace.js
+++ src/places/pages/updatePlace.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Input from '../../shared/components/FormElements/Input';
@@ -35,6 +35,7 @@ const DUMMY_PLACES = [
 ];
 
 const UpdatePlace = () => {
+    const [isLoading,setIsLoading] = useState(true);
     const placeId = useParams().placeId;
 
     const [formState, inputHandler, setFormData] = useForm({
@@ -61,7 +62,8 @@ const UpdatePlace = () => {
                 isValid: true
             }
         },
-            true);
+        true);
+        setIsLoading(false);
     }, [setFormData, identifiedPlace]);
 
 
@@ -76,7 +78,7 @@ const UpdatePlace = () => {
         </div>
     }
 
-    if (!formState.inputs.title.value) {
+    if (isLoading) {
         return <div className='center'>
             <h2>Loading..!</h2>
         </div>
