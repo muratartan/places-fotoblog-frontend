67e06213cdafd48c261fabb9e8d8deff31832842
Murat Artan <muratartan@hotmail.com>
2021-04-05 00:23:34 +0300

    set routes in an elegant way

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from 'react';
+import React, { useState, useCallback } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import NewPlace from './places/pages/NewPlace';
@@ -7,10 +7,10 @@ import MainNavigation from './shared/components/navigation/MainNavigaton/MainNav
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
-import {AuthContext} from './shared/context/auth-context';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
-   const [isLoggedIn,setIsLoggedIn] = useState(false);
+   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
    const login = useCallback(() => {
       setIsLoggedIn(true);
@@ -18,30 +18,50 @@ const App = () => {
    const logout = useCallback(() => {
       setIsLoggedIn(false);
    }, []);
-   
+
+   let routes;
+
+   if (isLoggedIn) {
+      routes = (
+        <Switch>
+           <Route path='/' exact>
+               <Users />
+            </Route>
+            <Route path='/:userId/places' exact>
+               <UserPlaces />
+            </Route>
+            <Route path='/places/new' exact>
+               <NewPlace />
+            </Route>
+            <Route path='/places/:placeId'>
+               <UpdatePlace />
+            </Route>
+            <Redirect to='/' />
+        </Switch>
+      );
+   } else {
+      routes = (
+         <Switch>
+             <Route path='/' exact>
+               <Users />
+            </Route>
+            <Route path='/:userId/places' exact>
+               <UserPlaces />
+            </Route>
+            <Route path='/auth'>
+               <Auth />
+            </Route>
+            <Redirect to='/auth' />
+         </Switch>
+      );
+   }
+
    return (
-      <AuthContext.Provider value={{isLoggedIn: isLoggedIn, login: login, logout: logout}}>
+      <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}>
          <Router>
             <MainNavigation />
             <main>
-               <Switch>
-                  <Route path='/' exact>
-                     <Users />
-                  </Route>
-                  <Route path='/:userId/places' exact>
-                     <UserPlaces />
-                  </Route>
-                  <Route path='/places/new' exact>
-                     <NewPlace />
-                  </Route>
-                  <Route path='/places/:placeId'>
-                     <UpdatePlace />
-                  </Route>
-                  <Route path='/auth'>
-                     <Auth />
-                  </Route>
-                  <Redirect to='/' />
-               </Switch>
+                  {routes}
             </main>
          </Router>
       </AuthContext.Provider>)
