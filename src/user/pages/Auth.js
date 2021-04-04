d0670ccac3e59300577185c5212349403b8a03cc
Murat Artan <muratartan@hotmail.com>
2021-04-04 22:06:11 +0300

    set form element

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 import Card from '../../shared/components/UIElements/Card/Card';
+import Input from '../../shared/components/FormElements/Input';
+import {VALIDATOR_EMAIL, VALIDATOR_MINLENGTH} from '../../Util/validator';
 import './Auth.css';
 
 const Auth = props => {
     return <Card className='authentication'>
         <h2>Login required!</h2>
         <hr/>
-        <form></form>
+        <form>
+            <Input 
+            element="input" 
+            id="email" 
+            type="email" 
+            label="E-mail" 
+            validators={[VALIDATOR_EMAIL]}
+            errorText="please enter a valid email"
+            onChange={}></Input>
+        </form>
     </Card>
 };
 
