a3cf95ad11c04fccd9f3b94a6e7583826fdeb246
Murat Artan <muratartan@hotmail.com>
2021-04-04 22:16:48 +0300

    set useForm

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -3,9 +3,22 @@ import React from 'react';
 import Card from '../../shared/components/UIElements/Card/Card';
 import Input from '../../shared/components/FormElements/Input';
 import {VALIDATOR_EMAIL, VALIDATOR_MINLENGTH} from '../../Util/validator';
+import {useForm} from '../../shared/hooks/form-hook';
 import './Auth.css';
 
-const Auth = props => {
+const Auth = (props) => {
+
+  const [formState, inputHandler] =  useForm({
+        email: {
+            value: '',
+            isValid: false
+        },
+        password: {
+            value: '',
+            isValid: false
+        }
+    },false)
+
     return <Card className='authentication'>
         <h2>Login required!</h2>
         <hr/>
@@ -17,7 +30,7 @@ const Auth = props => {
             label="E-mail" 
             validators={[VALIDATOR_EMAIL]}
             errorText="please enter a valid email"
-            onChange={}></Input>
+            onChange={inputHandler}></Input>
         </form>
     </Card>
 };
