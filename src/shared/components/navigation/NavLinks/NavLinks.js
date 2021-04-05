c15bb385aff9cbcf886cc19c077177fc25b6cc16
Murat Artan <muratartan@hotmail.com>
2021-04-05 14:44:59 +0300

    add logout button when authenticated

--- src/shared/components/navigation/NavLinks/NavLinks.js
+++ src/shared/components/navigation/NavLinks/NavLinks.js
@@ -32,6 +32,13 @@ const NavLinks = props => {
                 </li>
             )
         }
+        {
+            auth.isLoggedIn && (
+                <li>
+                    <button onClick={auth.logout}>LOGOUT</button>
+                </li>
+            )
+        }
     </ul>
 };
 
